0fee30e29e9dc14911e7bc01e512aed45abcd13e
Szymon Kurcab <skurcab@antennasoftware.com>
2012-10-10 19:36:04 +0200

    Status channel defined

--- server.rb
+++ server.rb
@@ -5,34 +5,36 @@ require 'json'
 
 @sockets = {}
 
-puts "Starting server on port 1111"
-
 EventMachine.run do
+
+  puts "Statring server on port 1111. You can talk now."
+  @status_channel = EM::Channel.new
 
   EventMachine::WebSocket.start(host: '0.0.0.0', port: 1111) do |socket|
 
     EventMachine.add_periodic_timer(10) do
-      @sockets.each { |id, s|
-        s.send({ msg: "ping"}.to_json)
-      }
+      @status_channel.push({ status: 'check'}.to_json)
     end
  
     socket.onopen do
-      @sockets.merge!(socket.object_id => socket)
-      puts "Opened socket with id: #{socket.object_id}"
-      socket.send({ msg: "Hello #{socket.object_id}!"}.to_json)
+      sid = @status_channel.subscribe { |msg| socket.send msg }
+      @sockets.merge!(sid => socket)
+      puts "Opened socket with id: #{sid}"
+      socket.send({ msg: "Hello #{sid}!"}.to_json)
       status
     end
 
     socket.onmessage do |msg|
-      js_msg = JSON.parse(msg)['msg']
-      puts "Message '#{js_msg}' received" 
-      socket.send({ msg: "Pong: #{msg.strip}"}.to_json)
+      js_msg = JSON.parse(msg)
+      puts "Message '#{js_msg['msg']}' received" 
+      socket.send({ msg: "Pong: #{js_msg['msg']}"}.to_json)
     end
 
     socket.onclose do
-      @sockets.delete socket.object_id
-      puts "Socket id: '#{socket.object_id}' has gone!"
+      sid = @sockets.key(socket)
+      @sockets.delete sid
+      @status_channel.unsubscribe(sid)
+      puts "Socket id: '#{sid}' has gone!"
       status
     end
 
